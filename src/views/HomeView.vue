feaf13e03788ae59434c8a4fecf691b5c97bc6ed
Gia Bao <giabao.231999@gmail.com>
2022-07-04 15:42:15 +0700

    dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,62 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app
+    id="inspire"
+    :style="{ background: $vuetify.theme.themes.dark.background }"
+  >
+    <SideBar />
+    <v-container>
+      <Navbar />
+      <v-row>
+        <v-col cols="12" sm="8">
+          <v-row>
+            <v-col cols="12" sm="12">
+              <Overview />
+            </v-col>
+            <v-col cols="12" sm="12">
+              <Money />
+            </v-col>
+            <v-col cols="12" sm="12">
+              <Payment />
+            </v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <Card />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import SideBar from "../components/SideBar.vue";
+import Navbar from "../components/Navbar.vue";
+import Overview from "../components/Overview.vue";
+import Money from "@/components/Money.vue";
+import Payment from "@/components/Payment.vue";
+import Card from "@/components/Card.vue";
 export default {
-  name: 'HomeView',
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "home",
+
   components: {
-    HelloWorld
-  }
-}
+    SideBar,
+    Navbar,
+    Overview,
+    Money,
+    Payment,
+    Card,
+  },
+};
 </script>
+
+<style >
+.v-btn.withoutupercase{
+  text-transform: none !important;
+
+}
+.v-btn:not(.v-btn--round).v-size--small{
+  min-width: 30px !important;
+  padding: 0 5px !important
+}
+</style>
